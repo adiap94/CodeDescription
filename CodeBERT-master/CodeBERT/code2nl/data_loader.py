f6a8bb92aa105053a0c1e4b0e372d8d7af77b19c
adi.a <adi.a@solocate.com>
2022-03-09 21:32:47 +0200

    not ready version - create CodeDataset and organize data loading section

---
+++ CodeBERT-master/CodeBERT/code2nl/data_loader.py
@@ -0,0 +1,169 @@
+import pandas as pd
+import random
+import shelve
+import torch
+import json
+from torch.utils.data import Dataset
+import logging
+
+
+
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt = '%m/%d/%Y %H:%M:%S',
+                    level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+class Example(object):
+    """A single training/test example."""
+
+    def __init__(self,
+                 idx,
+                 source,
+                 target,
+                 ):
+        self.idx = idx
+        self.source = source
+        self.target = target
+
+
+def read_examples(filename,debug_mode=False):
+    """Read examples from filename."""
+    # num = sum(1 for line in open(filename))
+    examples = []
+    with open(filename, encoding="utf-8") as f:
+        for idx, line in enumerate(f):
+            if debug_mode:
+                if idx == 20:
+                    break
+            line = line.strip()
+            js = json.loads(line)
+            if 'idx' not in js:
+                js['idx'] = idx
+            code = ' '.join(js['code_tokens']).replace('\n', ' ')
+            code = ' '.join(code.strip().split())
+            nl = ' '.join(js['docstring_tokens']).replace('\n', '')
+            nl = ' '.join(nl.strip().split())
+            examples.append(
+                Example(
+                    idx=idx,
+                    source=code,
+                    target=nl,
+                )
+            )
+    return examples
+
+
+class InputFeatures(object):
+    """A single training/test features for a example."""
+
+    def __init__(self,
+                 example_id,
+                 source_ids,
+                 target_ids,
+                 source_mask,
+                 target_mask,
+
+                 ):
+        self.example_id = example_id
+        self.source_ids = source_ids
+        self.target_ids = target_ids
+        self.source_mask = source_mask
+        self.target_mask = target_mask
+
+
+def convert_examples_to_features(examples, tokenizer, args, stage=None):
+    features = []
+    for example_index, example in enumerate(examples):
+        # source
+        source_tokens = tokenizer.tokenize(example.source)[:args.max_source_length - 2]
+        source_tokens = [tokenizer.cls_token] + source_tokens + [tokenizer.sep_token]
+        source_ids = tokenizer.convert_tokens_to_ids(source_tokens)
+        source_mask = [1] * (len(source_tokens))
+        padding_length = args.max_source_length - len(source_ids)
+        source_ids += [tokenizer.pad_token_id] * padding_length
+        source_mask += [0] * padding_length
+
+        # target
+        if stage == "test":
+            target_tokens = tokenizer.tokenize("None")
+        else:
+            target_tokens = tokenizer.tokenize(example.target)[:args.max_target_length - 2]
+        target_tokens = [tokenizer.cls_token] + target_tokens + [tokenizer.sep_token]
+        target_ids = tokenizer.convert_tokens_to_ids(target_tokens)
+        target_mask = [1] * len(target_ids)
+        padding_length = args.max_target_length - len(target_ids)
+        target_ids += [tokenizer.pad_token_id] * padding_length
+        target_mask += [0] * padding_length
+
+        if example_index < 5:
+            if stage == 'train':
+                logger.info("*** Example ***")
+                logger.info("idx: {}".format(example.idx))
+
+                logger.info("source_tokens: {}".format([x.replace('\u0120', '_') for x in source_tokens]))
+                logger.info("source_ids: {}".format(' '.join(map(str, source_ids))))
+                logger.info("source_mask: {}".format(' '.join(map(str, source_mask))))
+
+                logger.info("target_tokens: {}".format([x.replace('\u0120', '_') for x in target_tokens]))
+                logger.info("target_ids: {}".format(' '.join(map(str, target_ids))))
+                logger.info("target_mask: {}".format(' '.join(map(str, target_mask))))
+
+        features.append(
+            InputFeatures(
+                example_index,
+                source_ids,
+                target_ids,
+                source_mask,
+                target_mask,
+            )
+        )
+    return features
+
+
+
+class CodeDataset(Dataset):
+    def __init__(self,args,tokenizer,split):
+
+        self.args = args
+        self.tokenizer = tokenizer
+        self.debugBool=self.config["run_mode"]["debug_mode"]
+
+        self.split=split
+
+        # init dataset (has to be in the end)
+        self._load_entire_data()
+
+
+    def __len__(self):
+        return len(self.db)
+
+    def __getitem__(self, idx):
+        print(idx)
+        #
+        data = self.data[idx]
+        label = self.label[idx]
+
+        return (data, label)
+        return data
+
+
+    def _load_entire_data(self):
+        if self.split=="train":
+            filename = self.args.train_filename
+        elif self.split=="dev":
+            filename = self.args.dev_filename
+        elif  self.split=="test":
+            filename = self.args.test_filename
+        else:
+            raise Exception("NEED TO SELECT DATA SET")
+        # load data
+        examples = read_examples(filename,debug_mode=self.debugBool)
+        features = convert_examples_to_features(examples, self.tokenizer,self.args,stage=self.split)
+        all_source_ids = torch.tensor([f.source_ids for f in features], dtype=torch.long)
+        all_source_mask = torch.tensor([f.source_mask for f in features], dtype=torch.long)
+        all_target_ids = torch.tensor([f.target_ids for f in features], dtype=torch.long)
+        all_target_mask = torch.tensor([f.target_mask for f in features], dtype=torch.long)
+
+
+if __name__ == "__main__":
+    pass
--- CodeBERT-master/CodeBERT/code2nl/run.py
+++ CodeBERT-master/CodeBERT/code2nl/run.py
@@ -37,6 +37,7 @@ from itertools import cycle
 import torch.nn as nn
 from model import Seq2Seq
 from tqdm import tqdm, trange
+from data_loader import CodeDataset
 from torch.utils.data import DataLoader, Dataset, SequentialSampler, RandomSampler,TensorDataset
 from torch.utils.data.distributed import DistributedSampler
 from transformers import (WEIGHTS_NAME, AdamW, get_linear_schedule_with_warmup,
@@ -48,109 +49,6 @@ logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(messa
                     level = logging.INFO)
 logger = logging.getLogger(__name__)
 DEBUG_MODE = False
-class Example(object):
-    """A single training/test example."""
-    def __init__(self,
-                 idx,
-                 source,
-                 target,
-                 ):
-        self.idx = idx
-        self.source = source
-        self.target = target
-
-def read_examples(filename):
-    """Read examples from filename."""
-    # num = sum(1 for line in open(filename))
-    examples=[]
-    with open(filename,encoding="utf-8") as f:
-        for idx, line in enumerate(f):
-            if DEBUG_MODE:
-                if idx==20:
-                    break
-            line=line.strip()
-            js=json.loads(line)
-            if 'idx' not in js:
-                js['idx']=idx
-            code=' '.join(js['code_tokens']).replace('\n',' ')
-            code=' '.join(code.strip().split())
-            nl=' '.join(js['docstring_tokens']).replace('\n','')
-            nl=' '.join(nl.strip().split())            
-            examples.append(
-                Example(
-                        idx = idx,
-                        source=code,
-                        target = nl,
-                        )
-            )
-    return examples
-
-
-class InputFeatures(object):
-    """A single training/test features for a example."""
-    def __init__(self,
-                 example_id,
-                 source_ids,
-                 target_ids,
-                 source_mask,
-                 target_mask,
-
-    ):
-        self.example_id = example_id
-        self.source_ids = source_ids
-        self.target_ids = target_ids
-        self.source_mask = source_mask
-        self.target_mask = target_mask       
-        
-
-
-def convert_examples_to_features(examples, tokenizer, args,stage=None):
-    features = []
-    for example_index, example in enumerate(examples):
-        #source
-        source_tokens = tokenizer.tokenize(example.source)[:args.max_source_length-2]
-        source_tokens =[tokenizer.cls_token]+source_tokens+[tokenizer.sep_token]
-        source_ids =  tokenizer.convert_tokens_to_ids(source_tokens) 
-        source_mask = [1] * (len(source_tokens))
-        padding_length = args.max_source_length - len(source_ids)
-        source_ids+=[tokenizer.pad_token_id]*padding_length
-        source_mask+=[0]*padding_length
- 
-        #target
-        if stage=="test":
-            target_tokens = tokenizer.tokenize("None")
-        else:
-            target_tokens = tokenizer.tokenize(example.target)[:args.max_target_length-2]
-        target_tokens = [tokenizer.cls_token]+target_tokens+[tokenizer.sep_token]            
-        target_ids = tokenizer.convert_tokens_to_ids(target_tokens)
-        target_mask = [1] *len(target_ids)
-        padding_length = args.max_target_length - len(target_ids)
-        target_ids+=[tokenizer.pad_token_id]*padding_length
-        target_mask+=[0]*padding_length   
-   
-        if example_index < 5:
-            if stage=='train':
-                logger.info("*** Example ***")
-                logger.info("idx: {}".format(example.idx))
-
-                logger.info("source_tokens: {}".format([x.replace('\u0120','_') for x in source_tokens]))
-                logger.info("source_ids: {}".format(' '.join(map(str, source_ids))))
-                logger.info("source_mask: {}".format(' '.join(map(str, source_mask))))
-                
-                logger.info("target_tokens: {}".format([x.replace('\u0120','_') for x in target_tokens]))
-                logger.info("target_ids: {}".format(' '.join(map(str, target_ids))))
-                logger.info("target_mask: {}".format(' '.join(map(str, target_mask))))
-       
-        features.append(
-            InputFeatures(
-                 example_index,
-                 source_ids,
-                 target_ids,
-                 source_mask,
-                 target_mask,
-            )
-        )
-    return features
 
 
 
@@ -305,35 +203,24 @@ def main():
         csvLoggerFile_path = os.path.join(args.output_dir, "history.csv")
 
         # Prepare training data loader
-        train_examples = read_examples(args.train_filename)
-        train_features = convert_examples_to_features(train_examples, tokenizer,args,stage='train')
-        all_source_ids = torch.tensor([f.source_ids for f in train_features], dtype=torch.long)
-        all_source_mask = torch.tensor([f.source_mask for f in train_features], dtype=torch.long)
-        all_target_ids = torch.tensor([f.target_ids for f in train_features], dtype=torch.long)
-        all_target_mask = torch.tensor([f.target_mask for f in train_features], dtype=torch.long)    
-        train_data = TensorDataset(all_source_ids,all_source_mask,all_target_ids,all_target_mask)
-        
-        if args.local_rank == -1:
-            train_sampler = RandomSampler(train_data)
-        else:
-            train_sampler = DistributedSampler(train_data)
-        train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.train_batch_size//args.gradient_accumulation_steps)
-
+        print("loading training data")
+        train_dataset = CodeDataset(args=args,tokenizer=tokenizer,split = "train")
+        train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle=True)
         num_train_optimization_steps =  args.train_steps
 
+        # Start training
+        logger.info("***** Running training *****")
+        logger.info("  Num examples = %d", len(train_dataset))
+        logger.info("  Batch size = %d", args.train_batch_size)
+        logger.info("  Num epoch = %d", num_train_optimization_steps * args.train_batch_size // len(train_dataset))
+
         # create eval dataloader
-        eval_examples = read_examples(args.dev_filename)
-        eval_features = convert_examples_to_features(eval_examples, tokenizer, args, stage='dev')
-        all_source_ids = torch.tensor([f.source_ids for f in eval_features], dtype=torch.long)
-        all_source_mask = torch.tensor([f.source_mask for f in eval_features], dtype=torch.long)
-        all_target_ids = torch.tensor([f.target_ids for f in eval_features], dtype=torch.long)
-        all_target_mask = torch.tensor([f.target_mask for f in eval_features], dtype=torch.long)
-        eval_data = TensorDataset(all_source_ids, all_source_mask, all_target_ids, all_target_mask)
-        # dev_dataset['dev_loss'] = eval_examples, eval_data
-        eval_sampler = SequentialSampler(eval_data)
-        eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=args.eval_batch_size)
+        print("loading validation data")
+        eval_dataset = CodeDataset(args=args,tokenizer=tokenizer,split = "dev")
+        eval_loader = DataLoader(train_dataset, batch_size=args.eval_batch_size, shuffle=False)
+
         logger.info("\n***** Info evaluation *****")
-        logger.info("  Num examples = %d", len(eval_examples))
+        logger.info("  Num examples = %d", len(eval_dataset))
         logger.info("  Batch size = %d", args.eval_batch_size)
 
         # Prepare optimizer and schedule (linear warmup and decay)
@@ -347,12 +234,7 @@ def main():
         scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps,
                                                     num_training_steps=num_train_optimization_steps)
     
-        
-        #Start training
-        logger.info("***** Running training *****")
-        logger.info("  Num examples = %d", len(train_examples))
-        logger.info("  Batch size = %d", args.train_batch_size)
-        logger.info("  Num epoch = %d", num_train_optimization_steps*args.train_batch_size//len(train_examples))
+
         
 
         model.train()
