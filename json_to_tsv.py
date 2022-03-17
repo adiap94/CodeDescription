1e03511a4b4d2bcd890dd9d64422a8f5866a4e9f
Yavgenia Shteynman <jenny075@campus.technion.ac.il>
2022-03-17 19:04:16 +0200

    last_ver

--- json_to_tsv.py
+++ json_to_tsv.py
@@ -68,18 +68,18 @@ if __name__ == "__main__":
     print("Loading inputs...")
 
     has_baselines = False
-    data_path = '/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220315-161632'
-    TRANSFORMS = ['transforms.Identity', 'transforms.RenameParameters']
+    data_path = '/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-143412/'
+    TRANSFORMS = ['transforms.Identity', 'transforms.RenameParameters','transforms.RenameLocalVariables', 'transforms.RenameFields', 'transforms.AddDeadCode']
     for t_name in TRANSFORMS:
         loc = os.path.join(data_path, t_name)
         tasks = []
 
-        for split in ["test", "train", "valid"]:
-            if not os.path.isfile(os.path.join(loc + 'masked_'+split+'.jsonl.gz')):
+        for split in ["test"]:
+            if not os.path.isfile(os.path.join(loc + '/masked_'+split+'.jsonl.gz')):
                 continue
             if split == 'baseline':
                 has_baselines = True
-            for line in gzip.open(os.path.join(loc + 'masked_'+split+'.jsonl.gz')):
+            for line in gzip.open(os.path.join(loc + '/masked_'+split+'.jsonl.gz')):
                 as_json = json.loads(line)
                 from_file = as_json['from_file'] if 'from_file' in as_json else '{}.java'.format(as_json['sha256_hash'])
                 tasks.append((split, from_file, as_json['source_tokens'], as_json['target_tokens']))
@@ -87,11 +87,15 @@ if __name__ == "__main__":
         pool = multiprocessing.Pool()
         print("  + Inputs loaded")
 
+        # out_map = {
+        #     'test': open(os.path.join(loc , 'masked_token_test.tsv'), 'w'),
+        #     'train': open(os.path.join(loc , 'masked_token_train.tsv'), 'w'),
+        #     'valid': open(os.path.join(loc , 'masked_token_valid.tsv'), 'w')
+        # }
         out_map = {
             'test': open(os.path.join(loc , 'masked_token_test.tsv'), 'w'),
-            'train': open(os.path.join(loc , 'masked_token_train.tsv'), 'w'),
-            'valid': open(os.path.join(loc , 'masked_token_valid.tsv'), 'w')
         }
+
 
         if has_baselines:
             print("  + Has baselines file")
@@ -101,12 +105,12 @@ if __name__ == "__main__":
         print("  + Output files opened")
 
         out_map['test'].write('from_file\tsrc\ttgt\n')
-        out_map['train'].write('from_file\tsrc\ttgt\n')
-        out_map['valid'].write('from_file\tsrc\ttgt\n')
+        # out_map['train'].write('from_file\tsrc\ttgt\n')
+        # out_map['valid'].write('from_file\tsrc\ttgt\n')
 
         print("  - Processing in parallel...")
         iterator = tqdm.tqdm(
-            pool.imap_unordered(process, tasks, 1000),
+            pool.imap_unordered(process, tasks, 3000),
             desc="    - Tokenizing",
             total=len(tasks)
         )
