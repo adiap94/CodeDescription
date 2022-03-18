dc941bdf5a67aa77a36378b1ee91ff1e7b269c21
adi.a <adi.a@solocate.com>
2022-03-18 17:50:37 +0200

    modify augmentData2nnInput

--- augmentData2nnInput.py
+++ augmentData2nnInput.py
@@ -32,7 +32,7 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
     d_aug = {}
     for aug in augmentation_type_list:
         d_aug[aug] = []
-
+    counter = 0
     # alignment
     for index,row in df.iterrows():
 
@@ -42,9 +42,26 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
         source_info = df_source[df_source.code.str.contains(first_line, regex=False)]
         if len(source_info)== 0:
             continue
+        if len(source_info) ==1:
+            source_info = source_info.reset_index().iloc[0]
         if len(source_info)> 1:
-            print("need to debug")
-        source_info = source_info.reset_index().iloc[0]
+            counter = counter + 1
+            for line in lines[1:4]:
+                if line == '\n' or line == " " or line == "\t":
+                    continue
+                line_str =''.join(line).replace('\n', '')
+                line_str = ''.join(line_str).replace(' ', '')
+
+                for index_info, row_info in source_info.iterrows():
+                    str_jsonl = ''.join(row_info.code).replace(' ', '')
+                if line_str in str_jsonl:
+                    source_info = source_info.loc[index_info]
+                    print(counter)
+                    print("tsv:" +  row.src)
+                    print("source:" + row_info.code)
+
+
+
 
         for aug_type in augmentation_type_list:
             d = {}
@@ -58,10 +75,10 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
             d["docstring_tokens"] = source_info["docstring_tokens"]
 
             # save to jsonl
-            d_aug[aug].append(d)
+            d_aug[aug_type].append(d)
 
     for key, value in d_aug.items():
-        out_file = os.path.join("out_dir",key+".jsonl")
+        out_file = os.path.join(out_dir,key+".jsonl")
         with open(out_file, 'w') as outfile:
             json.dump(value, outfile)
 
@@ -72,7 +89,8 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
 if __name__ == "__main__":
     file_path ="/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340/final_test.tsv"
     source_path = "/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/test.jsonl"
-    dir ="/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340/transforms.Identity/test/"
+    Identity_dir ="/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340/transforms.Identity/test/"
+    out_dir = "/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340"
     out_file = "/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/tmp.jsonl"
-    alignAugmentData2Source(file_path,source_path)
+    alignAugmentData2Source(file_path,source_path,Identity_dir,out_dir)
     pass
