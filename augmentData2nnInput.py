dcd8782cf36f332ac6a578c0d948f4547e08030c
adi.a <adi.a@solocate.com>
2022-03-18 21:30:12 +0200

    fix saving jsonl in augmentData2nnInput

--- augmentData2nnInput.py
+++ augmentData2nnInput.py
@@ -20,14 +20,19 @@ def read_source_jsonl(source_path):
         df = df.append(d, ignore_index=True)
     return df
 
-def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
+def alignAugmentData2Source(tsv_path,source_path,out_dir=None):
+    os.makedirs(out_dir, exist_ok=True)
+    os.chmod(out_dir, mode=0o777)
+
     # source
+    print("about to load source json")
     df_source = read_source_jsonl(source_path)
+    print("finish loading source json")
 
     # augmentation
     df = pd.read_csv(tsv_path, sep='\t')
     df["number"] = df["filename"].apply(lambda x: int(x.split(".")[0]))
-    augmentation_type_list = df.columns.drop(["filename","src","tgt"])
+    augmentation_type_list = df.columns.drop(["filename","src","tgt","number"])
 
     # init
     d_aug = {}
@@ -36,6 +41,7 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
     counter = 0
     # alignment
     for index,row in df.iterrows():
+        print("| python_id_number is : " +  str(row.number))
         source_info = df_source.loc[row.number]
         if not row.tgt.split(" ")[0].lower() in source_info.func_name.lower():
             print("there is problem in index " + str(row.number))
@@ -43,7 +49,7 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
 
         for aug_type in augmentation_type_list:
             d = {}
-            code_str = "def "+ source_info.func_name +" "+ row[aug_type]
+            code_str = "def "+ str(source_info.func_name) +" "+ str(row[aug_type])
             code_tokens=code_str.split(" ")
 
             d["func_name"] = source_info.func_name
@@ -56,10 +62,13 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
             d_aug[aug_type].append(d)
 
     for key, value in d_aug.items():
-        out_file = os.path.join(out_dir,key+".jsonl")
+        out_file = os.path.join(out_dir,"test_"+key+".jsonl")
+        # with open(out_file, 'w') as outfile:
+        #     json.dump(value, outfile)
         with open(out_file, 'w') as outfile:
-            json.dump(value, outfile)
-
+            for entry in value:
+                json.dump(entry, outfile)
+                outfile.write('\n')
 
 
 
@@ -67,8 +76,6 @@ def alignAugmentData2Source(tsv_path,source_path,Identity_dir,out_dir=None):
 if __name__ == "__main__":
     file_path ="/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340/final_test.tsv"
     source_path = "/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/test.jsonl"
-    Identity_dir ="/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340/transforms.Identity/test/"
-    out_dir = "/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340"
-    out_file = "/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/tmp.jsonl"
-    alignAugmentData2Source(file_path,source_path,Identity_dir,out_dir)
+    out_dir = "/tcmldrive/project/resources/data_codesearch/CodeSearchNet/python/adv/adv_20220317-230340/data_jsonl/test"
+    alignAugmentData2Source(file_path,source_path,out_dir)
     pass
